24cc618a9ada3da8ed2edf450971074466220aff
MFactor1 <mingsqu@gmail.com>
2025-03-04 16:00:00 -0500

    Update test suite

--- my-app/src/test/java/com/mycompany/app/MinTest.java
+++ my-app/src/test/java/com/mycompany/app/MinTest.java
@@ -1,6 +1,5 @@
 package com.mycompany.app;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,28 +12,20 @@ import java.util.stream.Stream;
 
 public class MinTest {
 
-	private static Stream<Arguments> StandardTestArgs() {
+	private static Stream<Arguments> IntegerTestArgs() {
 		return Stream.of(
-				Arguments.of(2,1,3,1),
-				Arguments.of(-1,-2,-3,-3),
-				Arguments.of(-1,0,1,-1),
-				Arguments.of(1,1,1,1),
-				Arguments.of(-1,-1,-1,-1),
-				Arguments.of(0,0,0,0),
-				Arguments.of('b','a','c','a'),
-				Arguments.of('a','a','a','a'),
-				Arguments.of('2','1','3','1'),
-				Arguments.of("ab","aa","ac","aa"),
-				Arguments.of("aaa","aa","a","a"),
-				Arguments.of("2","1","3","1"),
-				Arguments.of("-1","-2","-3","-1"),
-				Arguments.of("-1","0","1","-1")
+			Arguments.of(2,1,3,1),
+			Arguments.of(-1,-2,-3,-3),
+			Arguments.of(-1,0,1,-1),
+			Arguments.of(1,1,1,1),
+			Arguments.of(-1,-1,-1,-1),
+			Arguments.of(0,0,0,0)
 		);
 	}
 
     @ParameterizedTest
 	@MethodSource("IntegerTestArgs")
-	public void StandardTest(int val1, int val2, int val3, int ans) {
+	public void IntegerTest(int val1, int val2, int val3, int ans) {
 		List<Integer> testList = new ArrayList<>();
 		testList.add(val1);
 		testList.add(val2);
@@ -42,7 +33,65 @@ public class MinTest {
 		assertEquals(Min.min(testList), ans);
 	}
 
+	private static Stream<Arguments> CharTestArgs() {
+		return Stream.of(
+			Arguments.of('b','a','c','a'),
+			Arguments.of('b','a','C','C'),
+			Arguments.of('a','a','a','a'),
+			Arguments.of('2','1','3','1')
+		);
+	}
+
+    @ParameterizedTest
+	@MethodSource("CharTestArgs")
+	public void CharTest(char val1, char val2, char val3, char ans) {
+		List<Character> testList = new ArrayList<>();
+		testList.add(val1);
+		testList.add(val2);
+		testList.add(val3);
+		assertEquals(Min.min(testList), ans);
+	}
+
+	private static Stream<Arguments> StringTestArgs() {
+		return Stream.of(
+			Arguments.of("ab","aa","ac","aa"),
+			Arguments.of("aB","aa","ac","aB"),
+			Arguments.of("aaa","aa","a","a"),
+			Arguments.of("2","1","3","1"),
+			Arguments.of("-1","-2","-3","-1"),
+			Arguments.of("-1","0","1","-1")
+		);
+	}
+
+    @ParameterizedTest
+	@MethodSource("StringTestArgs")
+	public void StringTest(String val1, String val2, String val3, String ans) {
+		List<String> testList = new ArrayList<>();
+		testList.add(val1);
+		testList.add(val2);
+		testList.add(val3);
+		assertEquals(Min.min(testList), ans);
+	}
+
 	@Test
-	public
+	public void SingleIntTest() {
+		List<Integer> testList = new ArrayList<>();
+		testList.add(2);
+		assertEquals(Min.min(testList), 2);
+	}
+
+	@Test
+	public void SingleCharTest() {
+		List<Character> testList = new ArrayList<>();
+		testList.add('b');
+		assertEquals(Min.min(testList), 'b');
+	}
+
+	@Test
+	public void SingleStringTest() {
+		List<String> testList = new ArrayList<>();
+		testList.add("test");
+		assertEquals(Min.min(testList), "test");
+	}
 
 }
